97c2cad2c34751ab4e0b3b42045c7abeb2223939
Robert James <rja@corti.ai>
2026-01-09 11:26:05 +0100

    poetry dynamic versioning + github release action workflow

--- src/bewer/__init__.py
+++ src/bewer/__init__.py
@@ -2,3 +2,10 @@ from bewer import core as core
 from bewer import metrics as metrics
 from bewer import preprocessing as preprocessing
 from bewer.core.dataset import Dataset  # noqa: F401
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("bewer")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+
